134e98ed189d3f423cb82e605fc45cf9a840790e
srihanrc <srihanrc@gmail.com>
2026-06-15 18:33:45 -0700

    Added summary of the 4 classes

---
+++ models.py
@@ -0,0 +1,9 @@
+"""
+Summary of the 4 classes:
+
+- Customer: manages customer information and order history.
+- FoodItem: represents the food available on the app with price, category, and population.
+- Catalog: organizes FoodItem objects into categories and allows for filtering.
+- Transaction: records purchases linking customers to items with totals and timestamps.
+"""
+
